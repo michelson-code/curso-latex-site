aa78c30e62e47b094d441de2a19a0caaf5ebbcce
bruno michelson <brunodantas@on.br>
2025-04-26 13:40:30 -0300

    Create criar_preferencia_mercadopago.sh

---
+++ criar_preferencia_mercadopago.sh
@@ -0,0 +1,19 @@
+curl --location --request POST 'https://api.mercadopago.com/checkout/preferences' \
+--header 'Authorization: Bearer APP_USR-2932416334016747-042609-d20f53cc9d7816e795fb8b0aeea2c2a1-2410853434' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+  "items": [
+    {
+      "title": "Curso de LaTeX",
+      "quantity": 1,
+      "currency_id": "BRL",
+      "unit_price": 24.90
+    }
+  ],
+  "back_urls": {
+    "success": "https://michelson-code.github.io/curso-latex-site/pagamento-sucesso.html",
+    "pending": "https://michelson-code.github.io/curso-latex-site/pagamento-pendente.html",
+    "failure": "https://michelson-code.github.io/curso-latex-site/pagamento-erro.html"
+  },
+  "auto_return": "approved"
+}'
